d92ea0ad36fc2d2578adec2ca5dd8ef54b2bf173
Matheus <math3uz1212@gmail.com>
2026-05-22 15:51:32 -0300

    any-to-many association with JoinTable

--- src/main/java/com/matheus/course/entities/Product.java
+++ src/main/java/com/matheus/course/entities/Product.java
@@ -6,35 +6,41 @@ import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
 	private Double price;
-	private String imgUml;
-	
+	private String imgUrl;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
-	
+
 	public Product() {
 	}
 
-	public Product(Long id, String name, String description, Double price, String imgUml) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
-		this.imgUml = imgUml;
+		this.imgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -69,12 +75,12 @@ public class Product implements Serializable{
 		this.price = price;
 	}
 
-	public String getImgUml() {
-		return imgUml;
+	public String getimgUrl() {
+		return imgUrl;
 	}
 
-	public void setImgUml(String imgUml) {
-		this.imgUml = imgUml;
+	public void setimgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
 	}
 
 	public Set<Category> getCategories() {
